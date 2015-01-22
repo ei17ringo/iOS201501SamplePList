d0a335ba5c90bbf8d2dff42253a0ef78b9ce70b5
ei17ringo <ei17ringo@gmail.com>
2015-01-22 10:09:07 +0800

    プロパティリスト（ファイル）から都道府県を読み込む

--- SamplePList/ViewController.m
+++ SamplePList/ViewController.m
@@ -9,14 +9,55 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSArray *_areaArray;
+}
 
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //_areaArray = @[@"北海道",@"青森県",@"岩手県",@"宮城県"];
+    
+    //プロジェクト内のファイルにアクセスできるオブジェクトを作成
+    NSBundle *bundle = [NSBundle mainBundle];
+    
+    //読み込むプロパティリストのファイルパス（場所）を指定
+    NSString *path = [bundle pathForResource:@"FriendsPerArea" ofType:@"plist"];
+    
+    //プロパティリストの中身のデータを取得
+    NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
+    
+    _areaArray = [dic objectForKey:@"AreaList"];
+    
+    
+    self.myTableView.dataSource = self;
+    self.myTableView.delegate = self;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _areaArray.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *CellIdetifier = @"Cell";
+    
+    //セルの再利用
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdetifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdetifier];
+    }
+    
+    cell.textLabel.text = _areaArray[indexPath.row];
+    
+    return cell;
+    
 }
 
 - (void)didReceiveMemoryWarning {
